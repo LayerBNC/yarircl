399895b6e32922a53f7ccb2a7c005406c37e13bd
Alexandre Oliveira <rockytvbr@gmail.com>
2016-11-22 15:38:46 -0200

    Update example

--- examples/user.rs
+++ examples/user.rs
@@ -4,37 +4,26 @@ use irc::*;
 use std::io::*;
 
 fn main() {
-    let mut user = IrcUser::new("rustybot1337", "yarircl", "Rust IRC Bot");
-    user.set_password("");
+    let mut user = IrcUser::new("RockyTV", "yarircl", "Rust IRC Bot");
+    user.set_password("1432");
 
     let mut client = IrcClient::new("chat.freenode.net:6667", user);
     let mut stream = client.connect();
     let mut buffer = String::new();
 
-    /*while stream.read_line(&mut buffer).unwrap() > 0 {
-        let msg = match IrcMessage::from_str(&buffer) {
-            Ok(x) => println!(">> {}", x.raw),
-            Err(e) => println!("error: {}", e)
-        };
-        buffer.clear();
-    }*/
-
-    /*loop {
-        let messages = match client.get_messages(&mut stream) {
-            Some(msgs) => {
-                for msg in msgs {
-                    println!("{}", msg.raw);
-                }
-            },
-            None => break
-        };
-    }*/
-
     let mut previous = 0;
     let mut current = 0;
     while client.conn_handler(&mut stream) {
-        println!("{:?}", client.messages[current].raw);
-        
+        let ref curr_msg = client.messages[current];
+        println!("{:?}", curr_msg.raw);
+
+        if curr_msg.command == NumericReply::PRIVMSG {
+            println!("{:?}", curr_msg);
+            if curr_msg.params[1] == "!ping" {
+                stream.send_message(&curr_msg.params[0], "Hello there!");
+            }
+        }
+
         previous = current;
         current += 1;
     }
