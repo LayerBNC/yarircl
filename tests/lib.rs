9edf392c6faa470c5191a13b8b76dd646d1d82a3
Alexandre Oliveira <rockytvbr@gmail.com>
2016-12-19 18:43:23 -0200

    Add more tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -28,10 +28,51 @@ fn test_irc_user_instantiate() {
         nickserv_password: String::from("password")
     };
 
-    let mut new_user = IrcUser::new("nick", "username", "real name");
+    let mut new_user = IrcUser::new("nick", "username");
+    new_user.set_realname("real name");
     new_user.set_password("password");
 
     println!("{:?}", new_user);
 
     assert_eq!(correct, new_user);
+}
+
+#[test]
+fn test_hostmask_from_str() {
+    let correct = Hostmask {
+        nick: String::from("Xinayder"),
+        user: String::from("~alex"),
+        host: String::from("unaffiliated/rockytv")
+    };
+    
+    let generated = Hostmask::from_str("Xinayder!~alex@unaffiliated/rockytv");
+
+    assert_eq!(correct, generated.unwrap());
+}
+
+#[test]
+#[should_panic]
+fn test_hostmask_from_invaid_str() {
+    let correct = Hostmask {
+        nick: String::from("Xinayder"),
+        user: String::from("~alex"),
+        host: String::from("unaffiliated/rockytv")
+    };
+    
+    let generated = Hostmask::from_str("~alex@unaffiliated/rockytv");
+
+    assert_eq!(correct, generated.unwrap());
+}
+
+#[test]
+fn test_hostmask_to_string() {
+    let hostmask = Hostmask {
+        nick: String::from("Xinayder"),
+        user: String::from("~alex"),
+        host: String::from("unaffiliated/rockytv")
+    };
+
+    let correct = String::from("Xinayder!~alex@unaffiliated/rockytv");
+    
+    assert_eq!(correct, hostmask.to_string());
 }
