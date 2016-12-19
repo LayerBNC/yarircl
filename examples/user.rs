8e0663ba8b2e546db9808059f076cd40dc032773
Alexandre Oliveira <rockytvbr@gmail.com>
2016-12-19 18:44:04 -0200

    Update example

--- examples/user.rs
+++ examples/user.rs
@@ -4,12 +4,14 @@ use irc::*;
 use std::io::*;
 
 fn main() {
-    let mut user = IrcUser::new("RockyTV", "yarircl", "Rust IRC Bot");
-    user.set_password("1432");
+    let mut user = IrcUser::new("RockyTV", "yarircl");
+    user.set_password("");
+    user.set_realname("Rust IRC Bot");
 
     let mut client = IrcClient::new("chat.freenode.net:6667", user);
     let mut stream = client.connect();
     let mut buffer = String::new();
+    let mut motd = false;
 
     let mut previous = 0;
     let mut current = 0;
@@ -21,6 +23,15 @@ fn main() {
             println!("{:?}", curr_msg);
             if curr_msg.params[1] == "!ping" {
                 stream.send_message(&curr_msg.params[0], "Hello there!");
+            }
+            else if curr_msg.params[1] == "!motd" {
+                println!("MOTD is:\r\n{}", client.server_motd);
+            }
+            else if curr_msg.params[1] == "!whois" {
+                stream.send_raw_message("WHOIS Phrohdoh");
+            }
+            else if curr_msg.params[1] == "!join" {
+                stream.join("#orcaware");
             }
         }
 
